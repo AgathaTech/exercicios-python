ac267914fcded2d7f71369bedd173a0b803cf57a
AgathaTech <agatiinha.ct@gmail.com>
2025-02-26 16:38:37 -0300

    Atualizando Ex002.py - adicionando código do exercício

---
+++ basico/Ex002.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua media.
+nota1 = float(input('Digite a sua primeira nota '))
+nota2 = float(input('Digite a sua segunda nota '))
+media = (nota1 + nota2) / 2
+print(f'Sua primeira nota é {nota1}, a sua segunda foi {nota2} e sua média é {media}')
