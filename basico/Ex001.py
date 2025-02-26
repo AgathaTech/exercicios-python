920dfc310a0713bd335b3324d77377fded5ffb20
AgathaTech <agatiinha.ct@gmail.com>
2025-02-26 12:46:20 -0300

    Adicionando .gitignore para ignorar arquivos do PyCharm

---
+++ basico/Ex001.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input("Digite um numero inteiro e mostre na tela o seu sucessor e seu antecessor: "))
+print(f"O numero é {num}, seu antecessor é {num-1} e seu sucessor é {num+1} ")
