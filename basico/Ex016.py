f24e9276e756a02eeab7909a2e29217e447ad65d
AgathaTech <agatiinha.ct@gmail.com>
2025-03-14 19:04:10 -0300

    Exercicio 016 - Programa que mostra contagem regressiva para estouro de fogos

---
+++ basico/Ex016.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa que mostre na tela uma contagem regressiva para o estouro
+# de fogos de artificio, inicio de 10 até 0, com uma pausa de 1 segundo entre eles.
+#acertei, faltou incrementar mais, mas poderia resumir com from time import sleep
+"""
+import time
+for c in range(10, 0, -1):
+    time.sleep(1)
+    print(c)
+print('POPOPOPAOPPPPPPPAPAPAAAAAAAAPPPPPPPPPOOOOOOOOO')
